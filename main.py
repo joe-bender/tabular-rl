9078b5a5efa66f71f581ba251156030fc70f28a3
Joe Bender <joebenderweb@gmail.com>
2018-10-23 07:17:03 -0400

    Add score averaging

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import gym
+from collections import deque
+import numpy as np
 from EpsilonDecay import EpsilonDecay
 from QLearningAgent import QLearningAgent
 
 environment = 'Taxi-v2'
 n_episodes = 1_000_000
+avg_window = 100
 
 alpha = 0.2
-epsilon = EpsilonDecay(1, 0, .999)
+epsilon = EpsilonDecay(1, 0, .9999)
 
 #-------------------------------------------------------------------------------
 
@@ -14,6 +17,7 @@ env = gym.make(environment)
 nA = env.action_space.n
 agent = QLearningAgent(nA, alpha, epsilon)
 
+latest_scores = deque(maxlen=avg_window)
 for i_episode in range(1, n_episodes+1):
     state = env.reset()
     action = agent.reset(state)
@@ -26,7 +30,8 @@ for i_episode in range(1, n_episodes+1):
         if done:
             break
     # print info
-    if i_episode % 100 == 0:
+    avg_score = np.mean(latest_scores)
+    if i_episode > avg_window:
         print('Episode: {}, '.format(i_episode), end='')
-        print('Score: {}, '.format(score), end='')
+        print('Avg Score: {}, '.format(score), end='')
         print('Epsilon: {}'.format(epsilon.value))
